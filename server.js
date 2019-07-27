83876d3c8e6f7e933653d8adbd0b8f12ea4e4cb3
Christian Villamin <ChristianVillamin31@gmail.com>
2019-07-28 07:02:23 +0800

    multer and res

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
-'use strict';
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
-var app = express();
+const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.get('/hello', function(req, res) {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const { originalname, mimetype, size } = req.file;
+
+  const metadata = {
+    name: originalname,
+    type: mimetype,
+    size: size
+  };
+
+  res.json(metadata);
+});
+
+app.listen(process.env.PORT || 3000, function() {
   console.log('Node.js listening ...');
 });
